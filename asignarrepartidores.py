2dc92202d98184a4bf1f8432b1566370d35f8904
royagresott03 <ragresottg21@campusuninunez.edu.co>
2026-02-19 18:21:07 -0500

    actu

--- asignarrepartidores.py
+++ asignarrepartidores.py
@@ -1,23 +1,27 @@
 def asignar_repartidor(zona_usuario, **repartidores):
     zona_usuario = zona_usuario.lower()
+    samuel = None
 
-    for nombre, zonas in repartidores.items():
+    for nombre, datos in repartidores.items():
+        zonas = datos["zonas"]
+        disponible = datos["disponible"]
         if zonas == "todas":
-            print("El repartidor asignado es:", nombre)
+            samuel = nombre
+            continue
+        if zona_usuario in [z.lower() for z in zonas] and disponible:
+            print("Repartidor asignado:", nombre)
             return
-        
-        if zona_usuario in [z.lower() for z in zonas]:
-            print("El repartidor asignado es:", nombre)
-            return
-
-    print("No hay repartidores disponibles para esa zona.")
+    if samuel:
+        print("Todos los repartidores de la zona están ocupados.")
+        print("Repartidor asignado:", samuel)
+    else:
+        print("No hay repartidores disponibles.")
 
 zona = input("Ingrese la zona de entrega: ")
-
 asignar_repartidor(
     zona,
-    Luis=["centro"],
-    Roy=["torices", "marbella"],
-    Marcos=["bicentenario"],
-    Samuel=["centro","torices","marbella","bicentenario"]
+    Luis={"zonas": ["centro"], "disponible": False},
+    Roy={"zonas": ["torices", "marbella"], "disponible": False},
+    Marcos={"zonas": ["bicentenario"], "disponible": True},
+    Samuel={"zonas": "todas", "disponible": True}
 )

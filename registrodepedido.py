0c2ef85acc7d222933d7e65d232cdd2cf0970f9b
royagresott03 <ragresottg21@campusuninunez.edu.co>
2026-02-19 18:21:26 -0500

    actu

--- registrodepedido.py
+++ registrodepedido.py
@@ -1,11 +1,11 @@
 def registrar_cliente():
-    print("--- Registro de cliente ---")
+    print("Registro de cliente")
     
     nombre = input("Ingrese su nombre: ")
     direccion = input("Ingrese su dirección: ")
     telefono = input("Ingrese su teléfono: ")
     referencia = input("Ingrese un dato de referencia: ")
-    
+
     datos_cliente = {
         "Nombre": nombre,
         "Dirección": direccion,
